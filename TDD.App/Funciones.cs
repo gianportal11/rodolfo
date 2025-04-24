9230c837620cc0f2ed4f5d551b0ee6363ae6129a
gianportal11 <n00462982@upn.pe>
2025-04-24 08:50:43 -0500

    Update Funciones.cs

--- TDD.App/Funciones.cs
+++ TDD.App/Funciones.cs
@@ -2,17 +2,22 @@ namespace TDD.App;
 
 public class Funciones
 {
-    public int Sumar(int dato1, int dato2)
+    public int? ObtenerMayor(int[] numeros)
     {
-        return dato1 + dato2;
-    }
+        if (numeros.Length == 0)
+        {
+            return null; // Retorna null si el arreglo está vacío
+        }
 
-    public bool EsPalindrome(string text)
-    {
-        var reverse = new string(text.Reverse().ToArray());
-        text = text.Replace(" ", "");
-        reverse = reverse.Replace(" ", "");
-        return reverse.ToLower().Equals(text.ToLower());
+        int mayor = numeros[0];
+        foreach (int num in numeros)
+        {
+            if (num > mayor)
+            {
+                mayor = num;
+            }
+        }
+        return mayor;
     }
-    
+}
 }
